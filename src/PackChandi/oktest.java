c83cc16d41a78a4c4e5f8a20f23aae78ab3f2612
sonasandeep20 <automationsandeep21@gmail.com>
2022-01-02 11:37:12 +0530

    second change

--- src/PackChandi/oktest.java
+++ src/PackChandi/oktest.java
@@ -1,5 +1,7 @@
 package PackChandi;
 
 public class oktest {
-
+  public static void main(String[] args) {
+	System.out.println();
+}
 }
---
+++ src/PackChandi/secondclass.java
@@ -0,0 +1,5 @@
+package PackChandi;
+
+public class secondclass {
+
+}
